e41d9dec101e5f3de91b66c43d27f515d9213911
Nathan Furnal <nathan_furnal@hotmail.com>
2021-04-29 22:37:24 +0200

    Correct positiongs

--- js/model/direction.js
+++ js/model/direction.js
@@ -35,7 +35,7 @@ class Direction {
 /**
  * Constants for the directions
  */
-Direction.NORTH = new Direction(1, 0);
-Direction.SOUTH = new Direction(-1, 0);
+Direction.NORTH = new Direction(-1, 0);
+Direction.SOUTH = new Direction(1, 0);
 Direction.WEST = new Direction(0, -1);
 Direction.EAST = new Direction(0, 1);
