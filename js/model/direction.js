6e90781afcfc89db8c2b3445624392433c738d5d
Nathan Furnal <nathan_furnal@hotmail.com>
2021-04-29 23:34:51 +0200

    Follow the rules on the directions

--- js/model/direction.js
+++ js/model/direction.js
@@ -5,21 +5,12 @@ class Direction {
   /**
    * Constructs a direction object with deltas on the row and column axes.
    *
-   * @param {number} deltaRow - the delta on the vertical axis
    * @param {number} deltaColumn - the delta on the horizontal axis
+   * @param {number} deltaRow - the delta on the vertical axis
    */
-  constructor(deltaRow, deltaColumn) {
-    this.__deltaRow = deltaRow;
+  constructor(deltaColumn, deltaRow) {
     this.__deltaColumn = deltaColumn;
-  }
-
-  /**
-   * Gets the row delta.
-   *
-   * @returns {number} the row delta
-   */
-  get deltaRow() {
-    return this.__deltaRow;
+    this.__deltaRow = deltaRow;
   }
 
   /**
@@ -30,12 +21,21 @@ class Direction {
   get deltaColumn() {
     return this.__deltaColumn;
   }
+
+  /**
+   * Gets the row delta.
+   *
+   * @returns {number} the row delta
+   */
+  get deltaRow() {
+    return this.__deltaRow;
+  }
 }
 
 /**
  * Constants for the directions
  */
-Direction.NORTH = new Direction(-1, 0);
-Direction.SOUTH = new Direction(1, 0);
-Direction.WEST = new Direction(0, -1);
-Direction.EAST = new Direction(0, 1);
+Direction.NORTH = new Direction(0, -1);
+Direction.SOUTH = new Direction(0, 1);
+Direction.WEST = new Direction(-1, 0);
+Direction.EAST = new Direction(1, 0);
