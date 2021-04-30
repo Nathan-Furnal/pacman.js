3c9a96e9956ea7698c67a8939b36125ed169f3e5
Nathan Furnal <nathan_furnal@hotmail.com>
2021-04-30 10:54:31 +0200

    Update jsDoc

--- js/view/pacmanView.js
+++ js/view/pacmanView.js
@@ -1,6 +1,19 @@
+/**
+ * Provides a view for the Pacman object, based on a controller. It allows the
+ * user to change the direction based on the player's input.
+ */
 class PacmanView {
+  /**
+   * Constructs the view based on the controller. It allows calls a `keywdown`
+   * jQuery function to allow key presses to update the direction of the Pacman.
+   *
+   * @param {PacmanCtrl} pacmanCtrl - the Pacman controller
+   */
   constructor(pacmanCtrl) {
     this.__pacmanCtrl = pacmanCtrl;
+    // arrow functions must be used as regular functions change what the keyword
+    // `this` refers to ; as arrow functions do not have prototypes yet explicit
+    // functions do.
     $("body").on("keydown", (event) => {
       switch (event.key) {
         case "ArrowUp":
